b1dfcb0bed5fe69da06d9a43c4afdb1387d2ee1e
Anthony Sawyer <sawyer.developer@gmail.com>
2023-10-26 18:10:15 -0400

    cleanup and prettier

--- App.tsx
+++ App.tsx
@@ -1,47 +1,42 @@
-import { StatusBar } from 'expo-status-bar'
-import { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from './src/designSystem/Button'
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from './src/designSystem/Button';
+import { GameView } from './src/components/GameView';
+import { ResultView } from './src/components/ResultView';
 
 export default function App() {
-
-  const [gameStarted, setGameStarted] = useState(false)
-  const [gameWon, setGameWon] = useState(null)
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameWon, setGameWon] = useState(null);
 
   const start = () => {
-    setGameStarted(true)
-  }
+    setGameStarted(true);
+  };
 
   const win = () => {
-    setGameStarted(false)
-    setGameWon(true)
-  }
+    setGameStarted(false);
+    setGameWon(true);
+  };
 
   const lose = () => {
-    setGameStarted(false)
-    setGameWon(false)
-  }
+    setGameStarted(false);
+    setGameWon(false);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.label}>
         This is{!gameStarted && ' going to be'} a fun game!
       </Text>
-      {!gameStarted ? <Button title='Start' onPress={start} /> :
-        <View>
-          <Button title='Win' onPress={win} />
-          <Button title='Lose' onPress={lose} />
-        </View>
-      }
-      {gameWon !== null && !gameStarted &&
-        <View>
-          <Text style={styles.label}>
-            You {gameWon ? 'Won!' : 'Lost!'}
-          </Text>
-        </View>}
-      <StatusBar style="dark" />
+      {!gameStarted ? (
+        <Button title='Start' onPress={start} />
+      ) : (
+        <GameView win={win} lose={lose} />
+      )}
+      {gameWon !== null && !gameStarted && <ResultView gameWon={gameWon} />}
+      <StatusBar style='dark' />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -49,9 +44,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#000'
+    backgroundColor: '#000',
   },
   label: {
-    color: '#fb9'
-  }
-})
+    color: '#fb9',
+  },
+});
