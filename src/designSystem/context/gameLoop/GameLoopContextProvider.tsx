72b0a79bb89431c7a3f11a316a77597b2892b4f5
Anthony Sawyer <sawyer.developer@gmail.com>
2023-11-12 21:13:39 -0500

    cleanup

--- src/designSystem/context/gameLoop/GameLoopContextProvider.tsx
+++ src/designSystem/context/gameLoop/GameLoopContextProvider.tsx
@@ -1,30 +1,31 @@
+import { useEffect } from 'react';
 import { GameLoopContext } from './GameLoopContext';
 
+const MAX_FPS = 60;
 export const GameLoopContextProvider = ({ children }) => {
-
-
-    const FPS = 60;
-    let prevTick = 0;    
-
-
-    const callbacks = []
-    const update = (callback) => {
-        console.log('update in provider')
-        callbacks.push(callback);
-    };
-    const callUpdates = () => {
-        requestAnimationFrame(callUpdates)
-        let now = Math.round(FPS * Date.now() / 1000);
-        if (now == prevTick) return;
-        prevTick = now;
-        for(var i = 0 ; i < callbacks.length - 1 ; i++){
-            callbacks[i]()
-        }
+  let prevTick = 0;
+  let callbacks = [];
+  const update = (callback) => {
+    console.log('update in provider', callback);
+    callbacks.push(callback);
+  };
+  const callUpdates = () => {
+    requestAnimationFrame(callUpdates);
+    let now = Math.round((MAX_FPS * Date.now()) / 1000);
+    if (now == prevTick) return;
+    prevTick = now;
+    for (var i = 0; i < callbacks.length - 1; i++) {
+      callbacks[i]();
     }
-    requestAnimationFrame(callUpdates)
+  };
+
+  useEffect(() => {
+    const req = requestAnimationFrame(callUpdates);
+    return () => cancelAnimationFrame(req);
+  }, [callUpdates]);
 
   return (
-    <GameLoopContext.Provider value={{update}}>
+    <GameLoopContext.Provider value={{ update }}>
       {children}
     </GameLoopContext.Provider>
   );
